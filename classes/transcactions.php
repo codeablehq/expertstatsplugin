e1e8a9a70f01223671297bfd39abafc9c50392f0
Panos | 11 Digit Labs <labs@11dig.it>
2017-01-23 16:06:16 +0200

    #1 - Added debug mode to hide the errors - Minor source code format - Removed closing php at the end

--- classes/transcactions.php
+++ classes/transcactions.php
@@ -1,47 +1,53 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+if (!defined('ABSPATH')) {
+  exit; // Exit if accessed directly
 }
 
-class wpcable_transcactions {
-  
+class wpcable_transcactions
+{
+
   private $api_calls = '';
+  private $debug = false;
   public $tables = array();
-  
-  
-  public function __construct() {
-    
-    global $wpdb;
-    
-    $this->tables = array(
-      'transcactions' => $wpdb->prefix .'codeable_transcactions',
-      'clients' => $wpdb->prefix . 'codeable_clients',
-      'amounts' => $wpdb->prefix . 'codeable_amounts'
-    );
-    
-    $this->api_calls = new wpcable_api_calls;
-    
-  }
-  
-  
-  // return the number of transcactions stored
-  public function store_transactions($maxpage = 999999) {
+
+
+  public function __construct()
+  {
 
     global $wpdb;
-    
-    $wpdb->show_errors();
+
+    $this->tables = array(
+      'transcactions' => $wpdb->prefix . 'codeable_transcactions',
+      'clients'       => $wpdb->prefix . 'codeable_clients',
+      'amounts'       => $wpdb->prefix . 'codeable_amounts'
+    );
+
+    $this->api_calls = new wpcable_api_calls;
+
+  }
+
+
+  // return the number of transcactions stored
+  public function store_transactions($maxpage = 999999)
+  {
+
+    global $wpdb;
+
+    if ($this->debug) {
+      $wpdb->show_errors();
+    }
+
     $account_details = $this->api_calls->login();
-    
+
     update_option('wpcable_account_details', $account_details);
-    
+
     $total = 0;
-    
-    $page = 1;
+    $page  = 1;
     while ($page < $maxpage) {
-      
+
       $single_page = $this->api_calls->transactions_page($page++);
-      
+
       if ($page == 2) {
         update_option('wpcable_average', $single_page['average_task_size']);
         update_option('wpcable_balance', $single_page['balance']);
@@ -51,55 +57,54 @@ class wpcable_transcactions {
       if (empty($single_page['transactions'])) {
         return $total;
       } else {
-        
+
         // get all data to the DB                
         foreach ($single_page['transactions'] as $tr) {
-          
+
           // check if transcactions already exists
-          $check = $wpdb->get_results("SELECT count(*) as totalrows FROM ". $this->tables['transcactions'] ." WHERE id = '".$tr['id']."'");
-          
+          $check = $wpdb->get_results("SELECT count(*) as totalrows FROM " . $this->tables['transcactions'] . " WHERE id = '" . $tr['id'] . "'");
+
           // if the record exists then return total
-          if($check[0]->totalrows > 0) {
-            
+          if ($check[0]->totalrows > 0) {
+
             if (get_option('wpcable_what_to_check') == 1) {
               continue;
             } else {
               return $total;
             }
           }
-          
+
           $new_tr = array(
-            'id'              => $tr['id'],
-            'description'     => $tr['description'],
-            'dateadded'       => date('Y-m-d H:i:s', $tr['timestamp']),
-            'fee_percentage'  => $tr['fee_percentage'],
-            'fee_amount'      => $tr['fee_amount'],
-            'task_type'       => $tr['task']['kind'],
-            'task_id'         => $tr['task']['id'],
-            'task_title'      => $tr['task']['title'],
-            'parent_task_id'  => ($tr['task']['parent_task_id'] > 0 ? $tr['task']['parent_task_id'] : 0),
-            'preferred'       => $tr['task']['current_user_is_preferred_contractor'],
-            'client_id'       => $tr['task_client']['id']
+            'id'             => $tr['id'],
+            'description'    => $tr['description'],
+            'dateadded'      => date('Y-m-d H:i:s', $tr['timestamp']),
+            'fee_percentage' => $tr['fee_percentage'],
+            'fee_amount'     => $tr['fee_amount'],
+            'task_type'      => $tr['task']['kind'],
+            'task_id'        => $tr['task']['id'],
+            'task_title'     => $tr['task']['title'],
+            'parent_task_id' => ($tr['task']['parent_task_id'] > 0 ? $tr['task']['parent_task_id'] : 0),
+            'preferred'      => $tr['task']['current_user_is_preferred_contractor'],
+            'client_id'      => $tr['task_client']['id']
           );
-          
+
           $insert_transcaction = $wpdb->insert(
             $this->tables['transcactions'],
             $new_tr
           );
-          
-          if ($insert_transcaction === false) {
-            die('could not insert transcactions '. $tr['id'].':'. $wpdb->print_error());
-          }
-          
-          
-          
-          // check if transcactions already exists
-          $check_client = $wpdb->get_results("SELECT count(*) as totalrows FROM ". $this->tables['clients'] ." WHERE client_id = '".$tr['task_client']['id']."'");
-          
-          // if the client record exists then return total
-          if(!($check_client[0]->totalrows > 0)) {
 
-            $new_client = array (
+          if ($insert_transcaction === false) {
+            die('could not insert transcactions ' . $tr['id'] . ':' . $wpdb->print_error());
+          }
+
+
+          // check if transcactions already exists
+          $check_client = $wpdb->get_results("SELECT count(*) as totalrows FROM " . $this->tables['clients'] . " WHERE client_id = '" . $tr['task_client']['id'] . "'");
+
+          // if the client record exists then return total
+          if (!($check_client[0]->totalrows > 0)) {
+
+            $new_client = array(
               'client_id'       => $tr['task_client']['id'],
               'full_name'       => $tr['task_client']['full_name'],
               'role'            => $tr['task_client']['role'],
@@ -111,15 +116,15 @@ class wpcable_transcactions {
               'medium'          => $tr['task_client']['avatar']['medium_url'],
               'large'           => $tr['task_client']['avatar']['large_url'],
             );
-            
+
             $insert_client = $wpdb->replace(
               $this->tables['clients'],
               $new_client
             );
-          
+
           }
-          
-          $new_amount = array (
+
+          $new_amount = array(
             'task_id'               => $tr['task']['id'],
             'client_id'             => $tr['task_client']['id'],
             'credit_revenue_id'     => $tr['credit_amounts'][0]['id'],
@@ -133,22 +138,20 @@ class wpcable_transcactions {
             'debit_user_id'         => $tr['debit_amounts'][1]['id'],
             'debit_user_amount'     => $tr['debit_amounts'][1]['amount'],
           );
-          
+
           $insert_amount = $wpdb->insert(
             $this->tables['amounts'],
             $new_amount
           );
-          
+
           $total++;
-          
-          
+
+
         }
-        
+
       }
     }
-    
-  }
-    
-}
 
-?>
+  }
+
+}
