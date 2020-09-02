c12e3fadc5fd638d8558a4d88184e5d06ae44e18
Panagiotis Synets <11digitlabs@users.noreply.github.com>
2020-09-02 10:28:33 +0300

    🚀 VERSION: Bump up version number

--- wp-codeable.php
+++ wp-codeable.php
@@ -5,7 +5,7 @@
  * Plugin Name:       Codeable - Expert's Stats
  * Plugin URI:        https://github.com/codeablehq/blackbook/wiki/Expert-Stats-Plugin
  * Description:       Get your Codeable data
- * Version:           0.2.0
+ * Version:           0.3.0
  * Author:            Spyros Vlachopoulos
  * Contributors:      Panagiotis Synetos, John Leskas, Justin Frydman, Jonathan Bossenger, Rob Scott, Philipp Stracker
  * Author URI:        https://codeable.io/developers/spyros-vlachopoulos/
